7773e1e92eb6ef07905f7ef1acd51a45c6a2fc9d
Alan Alonso <alan.alonso.gaf15@gmail.com>
2022-07-15 23:39:47 -0300

    update

--- src/components/RangeSlider.js
+++ src/components/RangeSlider.js
@@ -1,10 +1,13 @@
-import React ,{useState}from "react"
-
-function RangeSlider({defaultValue,setDefaultValue,onChange,vertical}){
-
-return(
-<input type='range' min='0' max='100' defaultValue={defaultValue} onChange={onChange}/>
-)
+function RangeSlider({ defaultValue, setDefaultValue, onChange, vertical }) {
+  return (
+    <input
+      type="range"
+      min="0"
+      max="100"
+      defaultValue={defaultValue}
+      onChange={onChange}
+    />
+  );
 }
 
-export default RangeSlider
+export default RangeSlider;
